db456a2cf42edd29525492ef4ec348e974404509
Abraham Kaczmarski <abrahamkaczmarski@gmail.com>
2021-11-29 22:52:39 +0100

    Implement custom symbols

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ import * as THREE from 'three'
 import App from './scripts/App'
 import buildTree from './scripts/Tree'
 
-let app
-let materials = null
-
 const global = {
-  scene: null
+  app: null,
+  materials: null,
+  scene: null,
+  symbols: null
 }
 
 const grassTexture = new THREE.TextureLoader().load('img/grass-round.png')
@@ -15,7 +15,7 @@ window.onload = init
 
 function init() {
   const c = document.querySelector('canvas')
-  app = new App(c, { grid: false, ambient: true, setup })
+  global.app = new App(c, { grid: false, ambient: true, setup })
   initUi()
 }
 
@@ -55,6 +55,7 @@ function initUi() {
 
   document.getElementById('btn-start').addEventListener('click', () => {
     createMaterials()
+    createSymbols()
     addTree()
   })
 
@@ -62,7 +63,7 @@ function initUi() {
     .getElementById('btn-add-row')
     .addEventListener('click', () => addCustomNode())
   addCustomNode({ name: 'X', color: '#332211', direction: 30, bend: 0 })
-  addCustomNode({ name: 'D', color: '#332211', direction: 70, bend: 30 })
+  addCustomNode({ name: 'D', color: '#332211', direction: 70, bend: 60 })
 }
 
 function addCustomNode({
@@ -87,13 +88,24 @@ function addCustomNode({
 }
 
 function createMaterials() {
-  materials = {}
+  global.materials = {}
   document.querySelectorAll('.custom-node').forEach(node => {
     const name = node.querySelector('.node-name').value
     const colorHex = node.querySelector('.node-color').value
     const color = parseInt(colorHex.substring(1), 16)
-    materials[name] = new THREE.MeshStandardMaterial({ color })
+    global.materials[name] = new THREE.MeshStandardMaterial({ color })
   })
+}
+
+function createSymbols() {
+  global.symbols = {}
+  document.querySelectorAll('.custom-node').forEach(node => {
+    const name = node.querySelector('.node-name').value
+    const direction = parseInt(node.querySelector('.node-y').value)
+    const bend = parseInt(node.querySelector('.node-x').value)
+    global.symbols[name] = { direction, bend }
+  })
+  console.log(global.symbols)
 }
 
 function rand(min, max) {
